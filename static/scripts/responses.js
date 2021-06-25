bad4aa5e7bd46fd07bffb55e06bd35939f8ca76e
MLaskun <mateo13311@gmail.com>
2021-06-25 14:04:40 +0200

    first commit2

---
+++ static/scripts/responses.js
@@ -0,0 +1,12 @@
+function getBotResponse(input){
+    switch(input) {
+        case "test":
+            return "to był test";
+            break;
+        case "prz":
+            return "politechnika";
+            break;
+        default:
+            return "Nie rozumiem";
+    }
+}
